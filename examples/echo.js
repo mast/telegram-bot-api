6b0f9e8648aae558381f30b9fd90aaf3152fb216
Phạm Ngọc Quang Nam <nampnq@gmail.com>
2015-07-02 08:37:01 +0700

    Update echo.js
    Keep 3 id latest in had_process to prevent memory leak

--- examples/echo.js
+++ examples/echo.js
@@ -10,11 +10,15 @@ var api = new telegram({
 var had_process = [];
 
 api.on('message', function(message) {
-	// Workaround duplicate message
+    // Workaround duplicate message
     if (had_process.indexOf(message.message_id) != -1) {
         return;
     }
+    if (had_process.length > 3){
+    	had_process.shift();
+    }
     had_process.push(message.message_id);
+    msg = message.text;
     api.sendMessage({
         chat_id: message.chat.id,
         text: msg
